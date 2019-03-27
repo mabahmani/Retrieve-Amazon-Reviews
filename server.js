2e226ea6a4a12b360b57564602d05faaf26e6396
Mohammad Amin Bahmani <aminbahmani10@gmail.com>
2019-03-28 00:21:47 +0430

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,9 @@ const server = http.createServer((req,res) => {
 	if (pageNum === null){
 		pageNum = '1'
 	}
-	console.log(isbn + pageNum)
-
 	if(req.url.startsWith('/api')){
 		console.log('Running test...')
 		const reviewsCrawler = require('./app.js')
-		console.log = ('after review crawler')
 		reviews = reviewsCrawler(isbn,pageNum)
 		//.then(console.log)
 		.catch(console.error)
